62fbab9d13a8be513074f74327d933c1254ef757
João Pedro de Faria <51465300+JoaoFaria025@users.noreply.github.com>
2022-11-07 17:21:06 -0300

    mudando CORS policy

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,7 @@ const routes = (app) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
         res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-        res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+        res.setHeader('Access-Control-Allow-Credentials', true); // If need
         res.send('cors problem fixed:)');
         return next();
     })
